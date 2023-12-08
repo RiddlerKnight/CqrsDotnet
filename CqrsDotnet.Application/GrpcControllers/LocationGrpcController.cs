9439241408d41e740c7cbf39c096b524e857aa3c
Bawornsit.C <puksigon@gmail.com>
2023-12-08 13:04:23 +0700

    update: gRPC controller example

--- CqrsDotnet.Application/GrpcControllers/LocationGrpcController.cs
+++ CqrsDotnet.Application/GrpcControllers/LocationGrpcController.cs
@@ -7,9 +7,12 @@ namespace CqrsDotnet.Application.GrpcControllers;
 
 public class LocationGrpcController : LocationGrpc.LocationGrpcBase
 {
-    public override Task<LocationReply> SyncLocation(LocationRequest request, ServerCallContext context)
+    public override async Task<LocationReply> SyncLocation(LocationRequest request, ServerCallContext context)
     {
+        // You can use CancellationToken via context
+        // context.CancellationToken;
+        
         Log.Information("Location | lat: {Lat} | long: {Long}", request.Lat, request.Long);
-        return Task.FromResult(new LocationReply() { Lat = "18.0101010", Long = "100.0101010"});
+        return new LocationReply() { Lat = "18.0101010", Long = "100.0101010"};
     }
 }
